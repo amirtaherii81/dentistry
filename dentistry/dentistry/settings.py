1b36fa2ba67759d6aab2ae571895f1ff2a1408c5
Fazel Momeni <wwwfazel88@gmail.com>
2024-11-13 18:12:14 +0330

    add js-select2 library and fix diseases searching ..

--- dentistry/dentistry/settings.py
+++ dentistry/dentistry/settings.py
@@ -83,17 +83,23 @@ WSGI_APPLICATION = 'dentistry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'db_dentistry3',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'db_dentistry3',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
